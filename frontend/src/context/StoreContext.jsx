1aab9d22af0f64e1cf66c5397f575434176dc685
srinivas <srinivaspotharaju89@gmail.com>
2025-07-19 17:44:31 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -8,7 +8,7 @@ export const StoreContext=createContext(null)
 const StoreContextProvider=(props) => {
 
     const [cartItems,setCartItems]=useState({});
-    const url="https://tomato-backend-vrjl.onrender.com";
+    const url="https://project-foodies.onrender.com";
     const [token ,setToken]=useState("");
     const [food_list,setFoodList]=useState([]);
 
